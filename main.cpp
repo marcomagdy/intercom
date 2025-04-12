8a621c4a66897df55683c8f84b1257ef4dde8058
Marco Magdy <m_magdy@apple.com>
2025-04-11 22:28:50 -0700

    Use printf instead of cerr and cout

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include <portaudio.h>
-#include <unistd.h>
-
 #include "TcpConnection.h"
+
+#include <portaudio.h>
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+#include <string>
 
 #define CHUNK_SIZE 1024
 #define SAMPLE_RATE 44100
@@ -23,7 +25,7 @@ int recordCallback(const void* inputBuffer, void* outputBuffer, unsigned long fr
         return paContinue; // Socket is full, continue recording
     }
     if (err != 0) {
-        std::cerr << "Error writing to socket: " << strerror(err) << std::endl;
+        printf("Error writing to socket: %s\n", strerror(err));
         return paComplete; // Stop recording on error
     }
     return paContinue;
@@ -42,7 +44,7 @@ int playCallback(const void* inputBuffer, void* outputBuffer, unsigned long fram
         return paContinue;                                          // No data available
     }
     if (err != 0) {
-        std::cerr << "Error reading from socket: " << strerror(err) << std::endl;
+        printf("Error reading from socket: %s\n", strerror(err));
         return paComplete; // Stop playback on error
     }
 
@@ -58,10 +60,10 @@ int playCallback(const void* inputBuffer, void* outputBuffer, unsigned long fram
     return paContinue; // Continue playback
 }
 
-class IntercomAudio
-{
+class IntercomAudio {
     PaStream* m_recording_stream;
     PaStream* m_playback_stream;
+
 public:
     IntercomAudio(PaStream* rec_stream, PaStream* play_stream)
         : m_recording_stream(rec_stream)
@@ -83,7 +85,8 @@ public:
 
     IntercomAudio(const IntercomAudio&) = delete;
     IntercomAudio& operator=(const IntercomAudio&) = delete;
-    IntercomAudio(IntercomAudio&& other) : m_recording_stream(other.m_recording_stream)
+    IntercomAudio(IntercomAudio&& other)
+        : m_recording_stream(other.m_recording_stream)
         , m_playback_stream(other.m_playback_stream)
     {
         other.m_recording_stream = nullptr;
@@ -102,14 +105,12 @@ public:
         inputParameters.hostApiSpecificStreamInfo = nullptr;
 
         PaStream* rec_stream;
-        auto err
-            = Pa_OpenStream(&rec_stream, &inputParameters, nullptr, SAMPLE_RATE, CHUNK_SIZE, paClipOff, recordCallback, &connection);
+        auto err = Pa_OpenStream(
+            &rec_stream, &inputParameters, nullptr, SAMPLE_RATE, CHUNK_SIZE, paClipOff, recordCallback, &connection);
         if (err != paNoError) {
-            std::cerr << "Error opening recording stream: " << Pa_GetErrorText(err) << std::endl;
+            printf("Error opening recording stream: %s\n", Pa_GetErrorText(err));
             return std::nullopt;
         }
-
-
 
         PaStreamParameters outputParameters;
         outputParameters.device = Pa_GetDefaultOutputDevice();
@@ -118,11 +119,11 @@ public:
         outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
         outputParameters.hostApiSpecificStreamInfo = nullptr;
 
-
         PaStream* play_stream;
-        err = Pa_OpenStream(&play_stream, nullptr, &outputParameters, SAMPLE_RATE, CHUNK_SIZE, paClipOff, playCallback, &connection);
+        err = Pa_OpenStream(
+            &play_stream, nullptr, &outputParameters, SAMPLE_RATE, CHUNK_SIZE, paClipOff, playCallback, &connection);
         if (err != paNoError) {
-            std::cerr << "Error opening playback stream: " << Pa_GetErrorText(err) << std::endl;
+            printf("Error opening playback stream: %s\n", Pa_GetErrorText(err));
             return std::nullopt;
         }
 
@@ -133,7 +134,7 @@ public:
     {
         auto err = Pa_StartStream(m_recording_stream);
         if (err != paNoError) {
-            std::cerr << "Error starting recording stream: " << Pa_GetErrorText(err) << std::endl;
+            printf("Error starting recording stream: %s\n", Pa_GetErrorText(err));
         }
     }
 
@@ -141,7 +142,7 @@ public:
     {
         auto err = Pa_AbortStream(m_recording_stream);
         if (err != paNoError) {
-            std::cerr << "Error stopping recording stream: " << Pa_GetErrorText(err) << std::endl;
+            printf("Error stopping recording stream: %s\n", Pa_GetErrorText(err));
         }
     }
 
@@ -149,7 +150,7 @@ public:
     {
         auto err = Pa_StartStream(m_playback_stream);
         if (err != paNoError) {
-            std::cerr << "Error starting playback stream: " << Pa_GetErrorText(err) << std::endl;
+            printf("Error starting playback stream: %s\n", Pa_GetErrorText(err));
         }
     }
 
@@ -157,10 +158,9 @@ public:
     {
         auto err = Pa_AbortStream(m_playback_stream);
         if (err != paNoError) {
-            std::cerr << "Error stopping playback stream: " << Pa_GetErrorText(err) << std::endl;
+            printf("Error stopping playback stream: %s\n", Pa_GetErrorText(err));
         }
     }
-
 };
 
 int extract_pid(std::string message)
@@ -196,19 +196,19 @@ std::string dicover_peer(bool& should_listen)
     snprintf(outgoing_message, sizeof(outgoing_message), broadcast_message, pid);
     std::string peer_ip_address;
     if (!optSocket) {
-        std::cerr << "Failed to create UDP socket" << std::endl;
+        printf("Failed to create UDP socket\n");
         return peer_ip_address;
     }
     auto& broadcastSocket = *optSocket;
 
     while (true) {
-
         broadcastSocket.broadcast((uint8_t*)outgoing_message, strlen(outgoing_message));
+        sleep(1); // Wait for 5 seconds before sending the next broadcast
         std::string sender_address;
         char incoming_msg[256];
         auto [read, err] = broadcastSocket.receive_from((uint8_t*)incoming_msg, sizeof(incoming_msg), sender_address);
         if (err != 0) {
-            std::cerr << "Error receiving broadcast: " << strerror(err) << std::endl;
+            printf("Error receiving broadcast: %s\n", strerror(err));
             continue;
         }
 
@@ -229,11 +229,10 @@ std::string dicover_peer(bool& should_listen)
             peer_ip_address = sender_address;
             break;
         }
-        usleep(100000); // Sleep for 100ms
     }
 
     if (peer_ip_address.empty()) {
-        std::cerr << "Failed to discover peer" << std::endl;
+        printf("Failed to discover peer\n");
     } else {
         printf("Discovered peer: %s\n", peer_ip_address.c_str());
     }
@@ -254,44 +253,38 @@ int main()
         printf("Connecting to peer...\n");
     }
 
-    
     Pa_Initialize();
     std::optional<Intercom::TcpConnection> connection;
     if (should_listen) {
-        // Start server
         printf("Starting server...\n");
         auto optListener = Intercom::TcpConnectionListener::listen(TCP_PORT);
         if (!optListener) {
-            std::cerr << "Failed to start listener" << std::endl;
+            printf("Failed to create listener\n");
             return -1;
         }
         auto& listener = *optListener;
 
         auto optConn = listener.accept();
         if (!optConn) {
-            std::cerr << "Failed to accept connection" << std::endl;
+            printf("Failed to accept connection\n");
             return -1;
         }
         optConn->set_non_blocking();
         connection = std::move(*optConn);
     } else {
-        // Start client
-        printf("Starting client...\n");
-        // Client code here
         printf("Connecting to [%s]...\n", peer_ip_address.c_str());
         if (auto optConn = Intercom::TcpConnection::connect(peer_ip_address.c_str(), TCP_PORT); optConn) {
             optConn->set_non_blocking();
             connection = std::move(*optConn);
-        }
-        else {
-            std::cerr << "Failed to connect to server" << std::endl;
+        } else {
+            printf("Failed to connect to [%s]\n", peer_ip_address.c_str());
             return -1;
         }
     }
 
     auto optIntercomAudio = IntercomAudio::create(*connection);
     if (!optIntercomAudio) {
-        std::cerr << "Failed to create audio stream" << std::endl;
+        printf("Failed to create audio streams\n");
         return -1;
     }
     auto& intercomAudio = *optIntercomAudio;
@@ -307,18 +300,16 @@ int main()
         intercomAudio.stop_recording();
         intercomAudio.stop_playback();
         char ch = input[0];
-        if (ch  == ' ') {
+        if (ch == ' ') {
             if (recording) {
                 printf("stop recording, start playback\n");
                 intercomAudio.start_playback();
-            }
-            else {
+            } else {
                 printf("start recording, stop playback\n");
                 intercomAudio.start_recording();
             }
             recording = !recording;
-        }
-        else if (ch == 'q') {
+        } else if (ch == 'q') {
             optIntercomAudio.reset();
             break;
         }
